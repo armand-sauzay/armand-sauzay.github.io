ce974dffeb4cb30120574c312bfcd1e425fdf57a
Armand Sauzay <665603asauzay@turo.com>
2022-05-22 16:40:56 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/armand-sauzay.github.io/precache-manifest.58862f9a0b485d103323204988e17e58.js"
+  "/armand-sauzay.github.io/precache-manifest.97bac948760982025b260e60fd52a85d.js"
 );
 
 workbox.clientsClaim();
