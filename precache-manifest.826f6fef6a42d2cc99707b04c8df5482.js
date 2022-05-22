458907dc4ff53c03bc6c5483f26b8d9462cc1eab
Armand Sauzay <665603asauzay@turo.com>
2022-05-22 16:25:28 -0700

    Updates

---
+++ precache-manifest.826f6fef6a42d2cc99707b04c8df5482.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2175549c18b903ed96c",
+    "url": "/armand-sauzay.github.io/static/js/main.d2175549.chunk.js"
+  },
+  {
+    "revision": "c08a46fcd19b9fca8418",
+    "url": "/armand-sauzay.github.io/static/js/runtime~main.c08a46fc.js"
+  },
+  {
+    "revision": "4a94c4e470dfd5e39bed6351c2230eea",
+    "url": "/armand-sauzay.github.io/index.html"
+  }
+];
